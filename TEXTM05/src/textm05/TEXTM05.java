2c6311798dd1c0b55cda739be3fe69d9dcda3d8c
ausias <ausias@hINFO6-27.xiam.cat>
2022-12-20 12:41:49 +0100

    Primer commit

--- TEXTM05/src/textm05/TEXTM05.java
+++ TEXTM05/src/textm05/TEXTM05.java
@@ -6,7 +6,8 @@ package textm05;
 
 /**
  *
- * @author ausias
+ * @author ausias,asaaodhjo
+
  */
 public class TEXTM05 {
 
@@ -15,6 +16,19 @@ public class TEXTM05 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Prueba 1
+        for(int i=0;i<=10;i++){
+            System.out.println("La i vale "+i);
+        }
+        //Prueba 2
+        for(int j=0;j<=20;j+=2){
+            System.out.println("La j vale "+j);
+        }
+        //Prueba 3
+        for(int i=0; i<30;i++) {
+        	System.out.println("La i vale: "+i);
+        }
     }
     
 }
+
